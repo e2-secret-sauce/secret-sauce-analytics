1439ed4ef3d51ffb4f308874344fab1435f3fb35
John Killmer <killmer.john@gmail.com>
2019-03-18 16:30:32 -0400

    Adding logic to check for violations

--- lambda_function.py
+++ lambda_function.py
@@ -26,6 +26,16 @@ def handler(event, context):
     array = BytesIO(response['Body'].read())
     for line in array:
         print(line)
+        line = line.decode("utf-8")
+        entry = line.split(",")
+        ticket_class = entry[5]
+        travel_type = entry[6]
+        print(ticket_class)
+        print(travel_type)
+      
+        if "coach" not in ticket_class and "domestic" in travel_type:
+            employee = entry[1]
+            logger.info(f'Violation Found [{employee}]')
     
     return {
         'statusCode': 200,
